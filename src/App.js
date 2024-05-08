b291994cfc2f6c09412950afbbb30314347023da
sth0824 <sth0824@naver.com>
2024-05-09 02:00:56 +0900

    2022/2/11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+  	return (
+    		<div className="components">
+      			<div className="div">
+        				<div className="tab">
+          					<div className="div1">뉴스</div>
+          					<div className="div1">교과서</div>
+        				</div>
+        				<div className="div3">
+          					<img className="group-icon" alt="" src="Group.svg" />
+          					<img className="group-icon1" alt="" src="Group.svg" />
+        				</div>
+        				<div className="div4">로그인</div>
+        				<div className="rectangle-parent">
+          					<div className="group-child" />
+          					<div className="search">Search...</div>
+          					<img className="magnify-icon" alt="" src="magnify.svg" />
+        				</div>
+      			</div>
+      			<div className="div5">
+        				<div className="tab">
+          					<div className="div1">뉴스</div>
+          					<div className="div1">교과서</div>
+        				</div>
+        				<div className="div3">
+          					<img className="group-icon" alt="" src="Group.svg" />
+          					<img className="group-icon1" alt="" src="Group.svg" />
+        				</div>
+        				<div className="rectangle-parent">
+          					<div className="group-child" />
+          					<div className="search">Search...</div>
+          					<img className="magnify-icon" alt="" src="magnify.svg" />
+        				</div>
+      			</div>
+      			<div className="div9">
+        				<div className="div10">이용약관</div>
+        				<div className="div11">고객센터</div>
+        				<div className="div12">개인정보처리방침</div>
+        				<div className="div13">사업자 정보확인</div>
+        				<img className="child" alt="" src="Vector 1.svg" />
+        				<img className="item" alt="" src="Vector 3.svg" />
+        				<img className="inner" alt="" src="Vector 2.svg" />
+      			</div>
+      			<div className="default">
+        				<div className="div14">버튼</div>
+      			</div>
+      			
+      			
+      			
+      			<div className="div21">
+        				<b className="b">제목</b>
+      			</div>
+      			<div className="div22">
+        				<div className="div1">부제목</div>
+      			</div>
+      			<div className="div24">내이름</div>
+      			<img className="icon" alt="" src="기본프로필 1.png" />
+      			<img className="components-child" alt="" src="Line 1.svg" />
+      			
+    		</div>);
+};
 
 export default App;
